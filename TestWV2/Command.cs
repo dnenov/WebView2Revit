0a8e9270630488659a417003f57b5fd27ad940ba
2adicted <dido.nenov@gmail.com>
2020-12-03 13:06:18 +0000

    Initial commit.

---
+++ TestWV2/Command.cs
@@ -0,0 +1,28 @@
+#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+#endregion
+
+namespace TestWV2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            Page pg = new Page();
+            pg.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ TestWV2/Page.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TestWV2
+{
+    /// <summary>
+    /// Interaction logic for Page.xaml
+    /// </summary>
+    public partial class Page : Window
+    {
+        bool _isNavigating = false;
+
+        public Page()
+        {
+            InitializeComponent();
+        }
+
+        void WebView_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            _isNavigating = true;
+            RequeryCommands();
+        }
+
+        void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            _isNavigating = false;
+            RequeryCommands();
+        }
+
+
+        void RequeryCommands()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
